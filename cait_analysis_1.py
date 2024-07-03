44f6906ce09cd268422a1161a39c531aee9feec5
m0rpholino <gregoriosons@gmail.com>
2024-07-03 10:04:06 +0200

    removed old files

--- cait_analysis_1.py
+++
@@ -1,8 +0,0 @@
-from main import mean_bin
-filenames = []
-
-baseline = 40
-bin_length = 5
-relevant_measures = ['Ti', 'TV', 'MV', 'PIF', 'AP']
-for file in filenames:
-    mean_bin(file, baseline, bin_length, relevant_measures)
